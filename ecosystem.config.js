ee195e0c04647bec0352306f49e3e4b35835eceb
ELBERT SALAS <elbertjsb@gmail.com>
2024-07-01 08:31:57 -0500

    se actualizado la configuracion del archivo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,17 @@
 module.exports = {
     apps: [
       {
-        name: 'API_WHATSAAP',
-        script: './src/app.ts',
+        name: "API_WHATSAAP",
+        script: "./src/app.ts",
         whatch: true,
         max_memory_restart: '1000M',
         exec_mode: "cluster",
         instance:1,
         env: {
-          NODE_ENV: 'production',
+          NODE_ENV: "development",
         },
         env_production: {
-          NODE_ENV: 'production',
-        },
-      },
-    ],
-  };
+          NODE_ENV: "production",
+        }
+      }]
+    }
